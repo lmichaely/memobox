899d150f599be3330d867b60fb7d5c18c7103aec
Ludwin Michaely <l_michaely@yahoo.com>
2025-04-14 15:11:02 +0800

    Restore original data.js with MongoDB logic

--- netlify/functions/test.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/test.js
-exports.handler = async (event, context) => {
-  console.log("Test function executed!");
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Test successful!" }),
-  };
-};
